c37af60ed63247bbab285ee40cb1d8eb774eb3c0
vn55d1z <vn55d1z@WEUS92315405667>
2023-01-28 16:17:10 -0300

    Agregar archivos de proyecto.

---
+++ EjercicioVariablesOperadores/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace EjercicioVariablesOperadores
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Boolean sabeOrnot;
+            Console.WriteLine("Ingrese nombre: ");
+            String? nombre = Console.ReadLine();
+            Console.WriteLine("Ingrese Apellido: ");
+            String? apellido = Console.ReadLine();
+            Console.WriteLine("Ingrese Edad: ");
+            String? edad = Console.ReadLine();
+            Console.WriteLine("Sabe programar? ");
+            String? programa= Console.ReadLine();
+            if (programa.Equals("si"))
+            {
+                Console.WriteLine("Nombre: " + nombre);
+                Console.WriteLine("Apellido: " + apellido);
+                Console.WriteLine("Edad: " + edad);
+                Console.WriteLine("Sabe programar...");
+            }
+            else
+            {
+                Console.WriteLine("Nombre: " + nombre);
+                Console.WriteLine("Apellido: " + apellido);
+                Console.WriteLine("Edad: " + edad);
+                Console.WriteLine("No sabe programar...");
+            }
+        }
+    }
+}
